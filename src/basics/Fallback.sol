e6c96291c9eaad37b0efe9ab8923869bb6f804aa
TilakMaddy <tilak.madichetti@gmail.com>
2024-05-15 19:07:28 +0530

    Fallback

---
+++ src/basics/Fallback.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract Fallback {
+
+    event Log1(string indexed funcName, address indexed who, uint indexed cash, bytes data);
+    event Log2(string indexed funcName, address indexed who, uint indexed cash);
+
+    /**
+        Fallback function is executed when the callee function doesn't exist
+        (or) when you directly send ETH to the contrat
+    */
+
+    // optionally payable
+    fallback() external payable  {
+        emit Log1("fallback", msg.sender, msg.value, msg.data);
+    }
+
+    /**
+        If money is send +  msg.data is empty,
+        receive is executed (if not declared it falls back to fallback())
+    */
+
+    // has to be payable
+    receive() external payable {
+        emit Log2("receive", msg.sender, msg.value);
+    }
+
+    function haha() public {
+        (bool success1,) = address(this).call{ value: 100 }("");
+        require(success1);
+    }
+
+}
+
+// send 2300 gas, bool returned
+// transfer 2300 gas, reverts
+// call - all gas, bool returned, bytes memory data returned
---
+++ src/basics/tests/Fallback.t.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity 0.8.19;
+
+import "lib/forge-std/src/Test.sol";
+import "../Fallback.sol";
+import "lib/forge-std/src/Vm.sol";
+
+contract FallbackTest is Test {
+
+    Fallback fk;
+
+    function setUp() external {
+        fk = new Fallback();
+        vm.deal(address(this), 1000);
+    }
+
+    error CallToNonExistentMethodFailed();
+
+    function test_invokeUnknownMethod() public {
+        (bool success1,) = address(fk).call{ value: 100 }(abi.encodeWithSignature("hello()"));
+        if (!success1) {
+            revert CallToNonExistentMethodFailed();
+        }
+        fk.haha();
+    }
+
+}
