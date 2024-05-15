c8e0d1db532c41a22a0142848b9b980dd5406d5e
TilakMaddy <tilak.madichetti@gmail.com>
2024-05-15 16:45:05 +0530

    imm visib

---
+++ src/basics/Immut.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract Ownable {
+    address payable public immutable owner = payable(msg.sender);
+
+    modifier onlyOwner {
+        require(msg.sender == owner, "You are not allowed!");
+        _;
+    }
+}
+
+contract Me is Ownable {
+    
+    event Log(address who);
+
+    function something() public onlyOwner {
+        emit Log(owner); // emitting to the blockchain is a tx
+    }
+}
---
+++ src/basics/Visib.sol
@@ -0,0 +1,5 @@
+
+/*
+    internal - inside the contract + child contracts
+    external - only from outside the conract (not even the child contract)
+*/
