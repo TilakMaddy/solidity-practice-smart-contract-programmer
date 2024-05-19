bfc8e73461bab132797edbd48cc596183fe5b21b
TilakMaddy <tilak.madichetti@gmail.com>
2024-05-19 07:43:07 +0530

    func selector

---
+++ src/intermediate/FuncSelector.sol
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.19;
+
+contract FunctionSelector {
+    function getSelector(string calldata _func) external pure returns(bytes4) {
+        return bytes4(keccak256(bytes(_func)));
+    }
+     function getSelector2(string calldata _func) external pure returns(bytes4) {
+        return bytes4(keccak256(abi.encodePacked(_func)));
+    }
+}
+
+contract Receiver {
+    event Log(bytes data);
+
+    function transfer(address _to, uint _amount) external {
+        emit Log(msg.data);
+    }
+}
+
+/*
+contract WontCompile {
+
+	function collate_propagate_storage(bytes16 x) external {
+	}
+
+	function burn(uint256 amount) external {}
+}*/
